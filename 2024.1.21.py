3567156e8309bca91fc724bfadfbeb79100555f0
xqm32 <458173774@qq.com>
2024-01-20 00:04:28 +0800

    leetcode: finished more

---
+++ 2024.1.21.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode.cn id=378 lang=python3
+#
+# [378] 有序矩阵中第 K 小的元素
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        n = len(matrix)
+
+        def check(mid):
+            i, j = n - 1, 0
+            num = 0
+            while i >= 0 and j < n:
+                if matrix[i][j] <= mid:
+                    num += i + 1
+                    j += 1
+                else:
+                    i -= 1
+            return num >= k
+
+        left, right = matrix[0][0], matrix[-1][-1]
+        while left < right:
+            mid = (left + right) // 2
+            if check(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
+
+
+# @lc code=end
