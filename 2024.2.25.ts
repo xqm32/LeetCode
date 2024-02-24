b8c008eb3a85aafecc314d87e73f5447a8ebd9be
xqm32 <458173774@qq.com>
2024-02-24 19:21:36 +0800

    leetcode: finished more

---
+++ 2024.2.25.ts
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=456 lang=typescript
+ *
+ * [456] 132 模式
+ */
+
+// @lc code=start
+function find132pattern(nums: number[]): boolean {
+  const n = nums.length;
+  const candidate_k = [nums[n - 1]];
+  let max_k = -Number.MAX_SAFE_INTEGER;
+
+  for (let i = n - 2; i >= 0; i--) {
+    if (nums[i] < max_k) {
+      return true;
+    }
+    while (
+      candidate_k.length &&
+      nums[i] > candidate_k[candidate_k.length - 1]
+    ) {
+      max_k = candidate_k[candidate_k.length - 1];
+      candidate_k.pop();
+    }
+    if (nums[i] > max_k) {
+      candidate_k.push(nums[i]);
+    }
+  }
+  return false;
+}
+// @lc code=end
