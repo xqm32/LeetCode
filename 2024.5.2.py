42b0e1b40cac8aa6c5ec77dfa07b30aa78e1929c
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2024-05-02 00:20:56 +0800

    leetcode: finished more

---
+++ 2024.5.2.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=235 lang=python3
+#
+# [235] 二叉搜索树的最近公共祖先
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return root
+
+
+# @lc code=end
