634071640780be4462abe55b0ddb5e7deed95852
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2024-05-03 00:29:58 +0800

    leetcode: finished more

---
+++ 2024.5.3.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode.cn id=73 lang=python3
+#
+# [73] 矩阵置零
+#
+
+# @lc code=start
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        row = set()
+        col = set()
+        m = len(matrix)
+        n = len(matrix[0])
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    row.add(i)
+                    col.add(j)
+        for i in row:
+            for j in range(n):
+                matrix[i][j] = 0
+        for j in col:
+            for i in range(m):
+                matrix[i][j] = 0
+
+
+# @lc code=end
