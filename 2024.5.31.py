a843014a480a699395947ea6823a5f38d7430cb0
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2024-05-31 01:41:05 +0800

    leetcode: finished more

---
+++ 2024.5.31.py
@@ -0,0 +1,43 @@
+#
+# @lc app=leetcode.cn id=138 lang=python3
+#
+# [138] 随机链表的复制
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+
+class Solution:
+    def copyRandomList(self, head: "Optional[Node]") -> "Optional[Node]":
+        if not head:
+            return None
+        cur = head
+        while cur:
+            new_node = Node(cur.val)
+            new_node.next = cur.next
+            cur.next = new_node
+            cur = new_node.next
+        cur = head
+        while cur:
+            cur.next.random = cur.random.next if cur.random else None
+            cur = cur.next.next
+        old_list = head
+        new_list = head.next
+        new_head = head.next
+        while old_list:
+            old_list.next = old_list.next.next
+            new_list.next = new_list.next.next if new_list.next else None
+            old_list = old_list.next
+            new_list = new_list.next
+        return new_head
+
+
+# @lc code=end
